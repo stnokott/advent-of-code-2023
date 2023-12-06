a4647ccf28426ba1c45981ae536ddfe707e7a231
stnokott <30934296+stnokott@users.noreply.github.com>
2023-12-06 18:12:31 +0100

    chore: add helper for reading lines from file

---
+++ internal/io/io.go
@@ -0,0 +1,23 @@
+package io
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+// ReadLines fully reads the file at path and returns a list of its lines.
+func ReadLines(path string) ([]string, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	s := strings.TrimSpace(string(data))
+	fmt.Println("read", len(data), "bytes")
+	// unify line terminators
+	s = strings.ReplaceAll(s, "\r\n", "\n")
+	lines := strings.Split(s, "\n")
+	fmt.Println("read", len(lines), "lines")
+	return lines, nil
+}
