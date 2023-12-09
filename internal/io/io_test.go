a51c92eb60eecb8536d490f21e82fc1d7fd54ec9
stnokott <30934296+stnokott@users.noreply.github.com>
2023-12-09 16:30:24 +0100

    test: add tests for package io

---
+++ internal/io/io_test.go
@@ -0,0 +1,35 @@
+package io
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestReadLines(t *testing.T) {
+	type args struct {
+		path string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []string
+		wantErr bool
+	}{
+		{"unix", args{"testdata/input_unix.txt"}, []string{"I have", "multiple lines", "with", "unix", "line terminator"}, false},
+		{"windows", args{"testdata/input_windows.txt"}, []string{"I have", "multiple lines", "with", "windows", "line terminator"}, false},
+		{"additional newline", args{"testdata/input_with_newline.txt"}, []string{"I have", "multiple lines", "and an", "additional", "line terminator", "at the end"}, false},
+		{"not found", args{"testdata/i_dont_exist.txt"}, nil, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := ReadLines(tt.args.path)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ReadLines() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ReadLines() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
