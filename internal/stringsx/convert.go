ecd8fab007ef4486cd847b2cfdf0e05778c1e2bc
stnokott <30934296+stnokott@users.noreply.github.com>
2023-12-16 16:54:30 +0100

    fix: trim spaces before converting string to int

--- internal/stringsx/convert.go
+++ internal/stringsx/convert.go
@@ -1,10 +1,14 @@
 package stringsx
 
-import "strconv"
+import (
+	"strconv"
+	"strings"
+)
 
-// MustAtoi wraps strconv.Atoi and panics if it errors
+// MustAtoi converts a trimmed string to a number.
+// It panics if an errors occurs.
 func MustAtoi(s string) int {
-	n, err := strconv.Atoi(s)
+	n, err := strconv.Atoi(strings.TrimSpace(s))
 	if err != nil {
 		panic(err)
 	}
